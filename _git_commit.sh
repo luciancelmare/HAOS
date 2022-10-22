ff743eea8aa0b12208d1cd3148fa4b3909944e2f
Lucian Cel Mare <luciancelmare@gmail.com>
2022-10-22 16:35:09 +0000

    autocommit changes

--- _git_commit.sh
+++ _git_commit.sh
@@ -2,7 +2,7 @@ cd /root/config
 #automatic add all files
 git add .
 #automatic commit with fixed name
-d=$(date +%Y%m%d_%hh%mm%ss)
+d=$(%Y%m%d_%hh%mm%ss)
 git commit -m "autocommit changes $d"
 #push to master
 git push origin master
